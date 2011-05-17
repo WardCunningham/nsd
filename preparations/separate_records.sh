bd16256d65862304620239465e1c7f4d3ca341a0
Ward Cunningham <ward@c2.com>
2011-05-16 17:47:33 -0700

    each subroutine is a record, find them all

--- preparations/separate_records.sh
+++ preparations/separate_records.sh
@@ -1,6 +1,7 @@
 mkdir -p ../exhibits/records
+rm ../exhibits/records/*
 cat ../exhibits/files/01 | 
 perl -ne '
-	open F, ">../exhibits/records/$1" if /^(DATA|STACK|BOX|SEGS|MOUSE|FILE|MAIN)/;
+	open F, ">../exhibits/records/$1" if /^([A-Z]{2,})/ and $1 ne "COMMON";
 	print F;
 '
