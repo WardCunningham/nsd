863ef92358e31716c7af11c56c4aad71365c1bce
WardCunningham <ward@c2.com>
2011-05-17 21:56:28 -0700

    include graph (based on file names)

--- preparations/make.sh
+++ preparations/make.sh
@@ -2,3 +2,4 @@ sh separate_files.sh
 sh separate_records.sh
 sh format_subroutines.sh
 sh plot_call_graph.sh
+sh plot_include_graph.sh
---
+++ preparations/plot_include_graph.sh
@@ -0,0 +1,28 @@
+cd ../exhibits
+mkdir -p graphs
+perl -e '
+	chdir "records";
+	print <<;
+digraph calls {
+node [shape=box,style=filled,fillcolor=gold];
+
+	for my $file (<*>) {
+		print stderr "$file\n";
+		@lines = `cat $file`;
+		shift @lines;
+		next if $lines[0] eq "COMMON\n";
+		for (@lines) {
+			next unless /^\*CALL,([A-Z]+)/;
+			if ($file eq "BLKDATA") {
+				print "$file -> \"/$1/\";\n";
+			} else {
+				print "\"/$1/\" -> $file;\n";
+			}
+		}
+		
+	}
+
+	print <<;
+}
+
+' > graphs/includes.dot
