fd16b4c2132ec5b639c1ec7f199bd7f4cbc37a32
Ward Cunningham <ward@c2.com>
2011-05-25 07:49:40 -0700

    get path to dot right by other means

--- development/plot_block_graphs.sh
+++ development/plot_block_graphs.sh
@@ -4,7 +4,5 @@ for i in *
 do echo $i
 	perl ../../../block_graph.pl $i |
 	tee ../../../data/graphs/blocks/$i.dot |
-	/usr/local/bin/dot -Tsvg -o ../../../../exhibits/graphs/blocks/$i.svg
+	dot -Tsvg -o ../../../../exhibits/graphs/blocks/$i.svg
 done
-
-# try /usr/local/bin/dot or /usr/bin/dot
