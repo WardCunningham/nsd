960493737a7e349db0a8c225218a8f3d800a304a
Ward Cunningham <ward@c2.com>
2011-06-06 08:13:35 -0700

    classify links by looking up words in available data

--- development/markup.pl
+++ development/markup.pl
@@ -1,6 +1,11 @@
 @lines = <>;
 for (@lines) {
-	s/\b[A-Z]+\b/<a href="#subroutines$&" class="subroutines">$&<\/a>/g;
+	s/\b[A-Z]+\b/anchor()/geo;
 	s/.+/<p>$&<\/p>/;
 }
 print @lines;
+
+sub anchor {
+	return "<a href=\"#subroutines$&\" class=\"subroutines\">$&</a>" if -f "data/records/subroutines/$&";
+	return $&;
+}
